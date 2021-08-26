4720556ab0b943d9a1ac40294df819e2b5f6a77f
ElonRM <t-ehmann@gmx.de>
2021-08-27 01:52:35 +0200

    Kapitel 6 finished

--- Kapitel_6_Wahrscheinlichkeit/Wahrscheinlichkeitsverteilungen.py
+++ Kapitel_6_Wahrscheinlichkeit/Wahrscheinlichkeitsverteilungen.py
@@ -3,34 +3,90 @@ import matplotlib.pyplot as plt
 import numpy
 
 
-def uniform_cdf(x: float) -> float:
-    """Wahrscheinlichkeit, dass eine gleichverteilte Zufallsgröße <=x ist"""
-    if x < 0:
-        return 0
-    elif x < 1:
-        return x
-    else:
-        return 1
+def plot_uniform_cdf():
+    def uniform_cdf(x: float) -> float:
+        """Wahrscheinlichkeit, dass eine gleichverteilte Zufallsgröße <=x ist"""
+        if x < 0:
+            return 0
+        elif x < 1:
+            return x
+        else:
+            return 1
+
+    xs = [x / 100.0 for x in range(-100, 200)]
+    plt.plot(xs, [uniform_cdf(x_i) for x_i in xs])
+    plt.legend()
+    plt.axis([-1, 2, -0.5, 2])
+    plt.title("The uniform cdf")
+    plt.show()
 
 
-xs = [x / 100.0 for x in range(-100, 200)]
-plt.plot(xs, [uniform_cdf(x_i) for x_i in xs])
-plt.legend()
-plt.axis([-1, 2, -0.5, 2])
-plt.title("The uniform cdf")
-plt.show()
+def plot_normal_pdf():
+    SQRT_TWO_PY = math.sqrt(2*math.pi)
+
+    def normal_pdf(x: float, mu: float = 0, sigma: float = 1) -> float:
+        return(math.exp(-(x-mu) ** 2 / (2 * sigma ** 2))/(sigma*SQRT_TWO_PY))
+
+    xs = [x/10 for x in range(-50, 50)]
+    plt.plot(xs, [normal_pdf(x, sigma=1)
+             for x in xs], '-', label='mu=0, sigma=1')
+    plt.plot(xs, [normal_pdf(x, sigma=2)
+             for x in xs], '--', label='mu=0, sigma=2')
+    plt.plot(xs, [normal_pdf(x, sigma=0.5)
+             for x in xs], ':', label='mu=0, sigma=0.5')
+    plt.plot(xs, [normal_pdf(x, mu=-1)
+             for x in xs], '-.', label='mu=-1, sigma=1')
+    plt.legend()
+    plt.title("Various normal pdfs")
+    plt.show()
 
 
-SQRT_TWO_PY = math.sqrt(2*math.pi)
+def plot_normal_cdf():
 
-def normal_pdf(x: float, mu: float = 0, sigma: float = 1) -> float:
-    return(math.exp(-(x-mu) ** 2 / (2 * sigma ** 2))/(sigma*SQRT_TWO_PY))
+    def normal_cdf(x: float, mu: float = 0, sigma: float = 1) -> float:
+        return(1 + math.erf((x-mu) / math.sqrt(2) / sigma)) / 2
 
-xs = [x/10 for x in range(-50,50)]
-plt.plot(xs, [normal_pdf(x,sigma = 1) for x in xs], '-', label = 'mu=0, sigma=1')
-plt.plot(xs, [normal_pdf(x,sigma = 2) for x in xs], '--', label = 'mu=0, sigma=2')
-plt.plot(xs, [normal_pdf(x,sigma = 0.5) for x in xs], ':', label = 'mu=0, sigma=0.5')
-plt.plot(xs, [normal_pdf(x,mu = -1) for x in xs], '-.', label = 'mu=-1, sigma=1')
-plt.legend()
-plt.title("Various normal pdfs")
-plt.show()
+    xs = [x/10 for x in range(-50, 50)]
+    plt.plot(xs, [normal_cdf(x) for x in xs], '-', label='mu: 0, sigma: 1')
+    plt.plot(xs, [normal_cdf(x, sigma=2)
+             for x in xs], '--', label='mu: 0, sigma: 2')
+    plt.plot(xs, [normal_cdf(x, sigma=0.5)
+             for x in xs], ':', label='mu: 0, sigma: 0.5')
+    plt.plot(xs, [normal_cdf(x, mu=-1)
+             for x in xs], '-.', label='mu: -1, sigma: 1')
+    plt.legend(loc=4)
+    plt.title("Various normal cdfs")
+    plt.show()
+
+
+def inverse_normal_cdf(p: float,
+                       mu: float = 0,
+                       sigma: float = 1,
+                       tolerance: float = 0.00001
+                       ) -> float:
+    """findet eine Näherung für den inversen Wert durch binäre Suche"""
+    
+    def normal_cdf(x: float, mu: float = 0, sigma: float = 1) -> float:
+        return(1 + math.erf((x-mu) / math.sqrt(2) / sigma)) / 2
+
+    #Wenn keine Standardverteilung vorliegt, skaliere zur SNV um
+    if mu != 0 or sigma != 1:
+        #Ist Z standardverteilt, so ist X = µ + Z*sigma normalverteilt
+        return mu + sigma * inverse_normal_cdf(p, tolerance= tolerance)
+    
+    low_z = -10.0
+    hi_z = 10.0
+    while hi_z-low_z > tolerance:
+        mid_z = (hi_z + low_z) / 2
+        print(hi_z, low_z, normal_cdf(mid_z))
+        if(normal_cdf(mid_z)> p):
+            hi_z = (hi_z+low_z)/2
+        else:
+            low_z = (hi_z+low_z)/2
+ 
+    return( (hi_z+low_z) /2)
+
+print(inverse_normal_cdf(p=0.8))  
+
+
+#plot_normal_cdf()
---
+++ Kapitel_6_Wahrscheinlichkeit/Zentrale_Grenzwertsatz.py
@@ -0,0 +1,42 @@
+from collections import Counter
+import math
+import random
+import matplotlib.pyplot as plt
+
+
+def bernulli_trial(p: float) -> int:
+    return 1 if random.random() < p else 0
+
+
+def binomial(n: int, p: float) -> int:
+    """Liefert die Summe von n Bernoulli(p)-Tests"""
+    return sum(bernulli_trial(p) for _ in range(n))
+
+
+def normal_cdf(x: float, mu: float = 0, sigma: float = 1) -> float:
+        return(1 + math.erf((x-mu) / math.sqrt(2) / sigma)) / 2
+
+
+def binomial_histogram(p: float, n: int, num_points: int) -> None:
+    """Wählt Punkte aus einem Binomial(n,p) und plottet deren Histogram"""
+    data = [binomial(n, p) for _ in range(num_points)]
+
+    histogram = Counter(data)
+    plt.bar([x - 0.4 for x in histogram.keys()], # -0.4, da die Blaken 0.8 breit sind
+            [v / num_points for v in histogram.values()],
+            0.8,
+            color='0.75')
+    
+    mu = n*p
+    sigma = math.sqrt(n*p*(1-p))
+
+    #Zeige neäherte Normalverteilung als Linie
+    xs = range(min(data), max(data)+1)
+    ys = [normal_cdf(x + 0.5, mu, sigma) - normal_cdf(x - 0.5, mu, sigma) for x in xs]
+    plt.plot(xs,ys)
+    plt.title("Boinomial Distribution vs. Normal Approximation")
+    plt.show()
+
+
+binomial_histogram(0.75, 100, 100000)
+
